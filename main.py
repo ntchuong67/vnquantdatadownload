d0adaa466af3a04c25aadcdf5b0e3d21a504d3a4
tienchuong <45691923+tienchuong@users.noreply.github.com>
2023-02-17 20:00:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from pypfopt import black_litterman
 from pypfopt import plotting
 import streamlit as st
 from datetime import datetime
-import vnquant 
+from vnquantdatadownload import vnquant 
 import matplotlib.pyplot as plt
 import copy
 import plotly.express as px
